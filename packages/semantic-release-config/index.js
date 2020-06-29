453c1e9e4b8b804a138039dddf5e4d6a603b35c1
Georgy Malanichev <georgy@malanichev.com>
2020-06-30 10:49:22 +1200

    style(semantic-release-config): fix linting errors

--- packages/semantic-release-config/index.js
+++ packages/semantic-release-config/index.js
@@ -1,27 +1,29 @@
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-			"@semantic-release/changelog",
-			{
-				"changelogTitle": "# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines."
-			}
-		],
-    "@semantic-release/github",
-    "@semantic-release/npm",
-    [
-			"@semantic-release/git",
-			{
-				"message": "chore(release): ${nextRelease.version} [skip ci]"
-			}
-		],
-    [
-      "semantic-release-slack-bot",
+      '@semantic-release/changelog',
       {
-        "notifyOnSuccess": true,
-        "notifyOnFail": true
-      }
-    ]
-  ]
-}
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+      },
+    ],
+  ],
+};
